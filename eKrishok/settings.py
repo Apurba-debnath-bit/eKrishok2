293fd271df32f27ffd3bc10b5ba077c1626fc0ca
Apurba-Debnath-bit <partho.bit999@gmail.com>
2021-10-03 02:18:23 +0600

    Allowed Host

--- eKrishok/settings.py
+++ eKrishok/settings.py
@@ -16,7 +16,9 @@ SECRET_KEY = 'django-insecure-_7@5#h)2yfd)6yi#8_a%f$e=(+z0q*wq8p-6&!xp4#z@tec%8x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ekrishok1.herokuapp.com'
+]
 
 
 # Application definition
